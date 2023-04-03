2fbf661b43a9e93c76a144c574805fc3738c6fdd
Matthias Reichenbach <matthreich@gmail.com>
2023-04-03 15:01:45 +0200

    refactor

--- src/frontend/resources/webview.js
+++ src/frontend/resources/webview.js
@@ -6,17 +6,17 @@ let refTags = [];
  * Event Listener that listens to all incoming messages from the extension.
  * Needs to be done so that Webview and js Script can communication with one another.
  */
-window.addEventListener('message', event => {
+window.addEventListener("message", (event) => {
   // Data send from extension
-  const message = event.data; 
+  const message = event.data;
 
-  switch(message.command) {
-    case 'updateButtons':
-      document.querySelector('#nextButton').disabled = !message.next;
-      document.querySelector('#prevButton').disabled = !message.prev;
-      document.querySelector('#lastButton').disabled = !message.last;
+  switch (message.command) {
+    case "updateButtons":
+      document.querySelector("#nextButton").disabled = !message.next;
+      document.querySelector("#prevButton").disabled = !message.prev;
+      document.querySelector("#lastButton").disabled = !message.last;
       break;
-    case 'updateContent':
+    case "updateContent":
       updateVisualization(message.traceElem);
       updateIntend(message.traceElem);
       updateRefArrows(message.traceElem);
@@ -26,7 +26,7 @@ window.addEventListener('message', event => {
 
 /**
  * Updates the Visualization in the Webview, with the given BackendTraceElem.
- * 
+ *
  * @param traceElem A BackendTraceElem with 3 fields (line, stack, heap)
  */
 function updateVisualization(traceElem) {
@@ -54,32 +54,35 @@ function updateVisualization(traceElem) {
       </div>
     </div>
   `;
-  document.getElementById('viz').innerHTML = data;
+  document.getElementById("viz").innerHTML = data;
 }
 
 /**
  * Updates the indendation for heap elements, if a other heap element references it.
- * 
+ *
  * @param traceElem A BackendTraceElem with 3 fields (line, stack, heap)
  */
 function updateIntend(traceElem) {
   const heapTags = traceElem[2].match(/(?<=startPointer)[0-9]+/g);
   if (heapTags) {
-    heapTags.forEach(tag => {
-      document.getElementById('objectItem' + tag).classList.add('object-intendation');
+    heapTags.forEach((tag) => {
+      document
+        .getElementById("objectItem" + tag)
+        .classList.add("object-intendation");
     });
   }
 }
 /**
  * Updates the Reference Arrows from frame items to heap & heap items to heap items.
- * 
+ *
  * @param traceElem A BackendTraceElem with 3 fields (line, stack, heap)
  */
 function updateRefArrows(traceElem) {
   const tags = getCurrentTags(traceElem);
-  refTags.forEach(tag => tag.remove());
+  refTags.forEach((tag) => tag.remove());
+
   if (tags) {
-    refTags = tags.map(tag => {
+    refTags = tags.map((tag) => {
       return new LeaderLine(tag.elem1, tag.elem2, {
         size: 2,
         path: "magnet",
@@ -89,7 +92,7 @@ function updateRefArrows(traceElem) {
         startSocketGravity: [50, -10],
         endSocketGravity: [-5, -5],
         endPlug: "arrow1",
-        color: getColor(tag)
+        color: getColor(tag),
       });
     });
   }
@@ -98,7 +101,7 @@ function updateRefArrows(traceElem) {
 /**
  * Retrieves all id's on the frame side and heap side, that have a potential start or end pointer in there id.
  * Is later used to create Reference Arrows.
- * 
+ *
  * @param traceElem A BackendTraceElem with 3 fields (line, stack, heap)
  * @returns A list with all ids that have either a start or end pointer id in the html
  */
@@ -107,20 +110,20 @@ function getCurrentTags(traceElem) {
   const heapTags = traceElem[2].match(/(?<=startPointer)[0-9]+/g);
   if (normalTags) {
     let s = [];
-    const t = normalTags.map(normalTag => {
+    const t = normalTags.map((normalTag) => {
       const id = normalTag.match(/(?<=.+)[0-9]+/g);
       return {
         tag: id,
         elem1: document.getElementById(normalTag),
-        elem2: document.getElementById('heapEndPointer' + id),
+        elem2: document.getElementById("heapEndPointer" + id),
       };
     });
     if (heapTags) {
-      s = heapTags.map(t => {
+      s = heapTags.map((t) => {
         return {
           tag: t,
-          elem1: document.getElementById('startPointer' + t),
-          elem2: document.getElementById('heapEndPointer' + t),
+          elem1: document.getElementById("startPointer" + t),
+          elem2: document.getElementById("heapEndPointer" + t),
         };
       });
     }
@@ -132,21 +135,21 @@ function getCurrentTags(traceElem) {
 
 /**
  * Creates a color based on the tag number (variable reference).
- * 
- * @param tag The tag number (variable reference) is used to create a color. 
- * @returns 
+ *
+ * @param tag The tag number (variable reference) is used to create a color.
+ * @returns
  */
 function getColor(tag) {
-  let hue = ((0.618033988749895 + (tag.tag / 10) ) % 1) * 100;
+  let hue = ((0.618033988749895 + tag.tag / 10) % 1) * 100;
   return `hsl(${hue}, 60%, 45%)`;
 }
 
 function onLoad() {
-  document.querySelector('#nextButton').disabled = false;
-  document.querySelector('#lastButton').disabled = false;
-  document.querySelector('#prevButton').disabled = true;
+  document.querySelector("#nextButton").disabled = false;
+  document.querySelector("#lastButton").disabled = false;
+  document.querySelector("#prevButton").disabled = true;
 }
 
 async function onClick(type) {
-  await vscode.postMessage({ command: 'onClick', type: type });
+  await vscode.postMessage({ command: "onClick", type: type });
 }
