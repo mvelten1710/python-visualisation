811860270f342ce73b6a19eeb9438dbf647fec55
Matthias Reichenbach <matthreich@gmail.com>
2023-05-09 11:29:18 +0200

    fixed when string is also a number

--- src/backend/JsonMapper.ts
+++ src/backend/JsonMapper.ts
@@ -1,25 +1,35 @@
 export function validJsonFor(type: HeapType, value: string): string {
+    let isActuallyString = false;
     return value.replace(/None|'|(\(|\))|(\{|\})|[0-9]+|(True|False)/g, (substring, _) => {
         switch (substring) {
             case 'None':
+                isActuallyString = false;
                 return '"None"';
             case "'":
+                isActuallyString = true;
                 return '"';
             case 'True':
+            case 'False':
+                isActuallyString = false;
                 return JSON.stringify(substring);
             case '{':
+                isActuallyString = false;
                 return type === 'set' ? '[' : '{';
             case '(':
+                isActuallyString = false;
                 return '[';
             case '}':
+                isActuallyString = false;
                 return type === 'set' ? ']' : '}';
             case ')':
+                isActuallyString = false;
                 return ']';
             default:
-                if (!isNaN(Number(substring))) {
+                if (!isNaN(Number(substring)) && !isActuallyString) {
+                    isActuallyString = false;
                     return JSON.stringify(substring);
                 }
-                return '';
+                return substring;
         }
     });
 }
