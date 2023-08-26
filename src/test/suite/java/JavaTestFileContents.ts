058b687e20fe8cf0190b2e515d6c37cf5717e1dc
Matthias Reichenbach <matthreich@gmail.com>
2023-08-26 15:29:35 +0200

    finished java tests

--- src/test/suite/java/JavaTestFileContents.ts
+++ src/test/suite/java/JavaTestFileContents.ts
@@ -19,3 +19,106 @@ public class JavaPrimitiveVariableTestClass {
     }
 }
 `;
+
+export const ALL_WRAPPER_VARIABLES = `
+public class JavaWrapperVariableTestClass {
+    public static void main(String[] args) {
+        Byte positiveByte = 1;
+        Byte negativeByte = -1;
+        Short positiveShort = 150;
+        Short negativeShort = -150;
+        Integer positiveInt = 55555;
+        Integer negativeInt = -55555;
+        Long positiveLong = 2947483647l;
+        Long negativeLong = -2947483647l;
+        Float positiveFloat = 1.0123f;
+        Float negativeFloat = -1.0123f;
+        Double positiveDouble = 1.01234d;
+        Double negativeDouble = -1.01234d;
+        Character fullChar = 'a';
+        Boolean trueBool = true;
+        Boolean falseBool = false;
+        String emptyString = "";
+        String fullString = "Hello World!";
+    }
+}
+`;
+
+export const ARRAY_VARIABLES = `
+public class JavaArrayVariableTestClass {
+    public static void main(String[] args) {
+        int[] intArray = { 1, 2, 3, 4};
+        Integer[] integerArray = { 1, 2, 3, 4};
+        int[][] dimIntArray = { {1}, {2}, {3, 4}};
+        Integer[][] dimIntegerArray = { {1}, {2}, {3, 4}};
+    }
+}
+`;
+
+export const CLASS_VARIABLES = `
+public class JavaClassTestClass {
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass(1d, 2d);
+    }
+}
+
+class TestClass {
+    private double x;
+    private Double y;
+
+    TestClass(double x, Double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+`;
+
+export const ARRAY_LIST_VARIABLES = `
+import java.util.List;
+import java.util.ArrayList;
+public class JavaArrayListTestClass {
+    public static void main(String[] args) {
+        List<Double> arrayList = new ArrayList<>();
+        arrayList.add(3d);
+        arrayList.add(2d);
+        arrayList.add(1d);
+    }
+}
+`;
+
+export const LINKED_LIST_VARIABLES = `
+import java.util.List;
+import java.util.LinkedList;
+public class JavaLinkedListTestClass {
+    public static void main(String[] args) {
+        List<Double> linkedList = new LinkedList<>();
+        linkedList.add(3d);
+        linkedList.add(2d);
+        linkedList.add(1d);
+    }
+}
+`;
+
+export const HASH_MAP_VARIABLES = `
+import java.util.Map;
+import java.util.HashMap;
+public class JavaHashMapTestClass {
+    public static void main(String[] args) {
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("Audi", 1);
+        hashMap.put("BMW", 2);
+    }
+}
+`;
+
+export const HASH_SET_VARIABLES = `
+import java.util.Set;
+import java.util.HashSet;
+public class JavaHashSetTestClass {
+    public static void main(String[] args) {
+        Set<Integer> hashSet = new HashSet<>();
+        hashSet.add(1);
+        hashSet.add(2);
+    }
+}
+`;
--- src/test/suite/java/extensionJava.test.ts
+++ src/test/suite/java/extensionJava.test.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 import { TESTFILE_DIR_JAVA, TestExecutionHelper, executeExtension, loadTraceFromContext } from '../TestExecutionHelper';
 import * as TestFileContents from './JavaTestFileContents';
 
-const TENTY_SECONDS = 50000;
+const MAX_TEST_DURATION = 30000;
 
 suite('The Backend handling a java file when', () => {
     after(() => {
@@ -14,7 +14,7 @@ suite('The Backend handling a java file when', () => {
     });
 
     describe("creating a Trace with all primitive Variables", function () {
-        this.timeout(TENTY_SECONDS);
+        this.timeout(MAX_TEST_DURATION);
 
         let result: BackendTrace | undefined;
         this.beforeAll(async function () {
@@ -56,8 +56,307 @@ suite('The Backend handling a java file when', () => {
                 if (!stack) {
                     assert.fail("No Stack Elements");
                 }
-               
+
                 stackContainsVariable([name, type, value], stack);
+            });
+        });
+    });
+
+    describe("creating a Trace with all Wrapper-Variables", function () {
+        this.timeout(MAX_TEST_DURATION);
+
+        let result: BackendTrace | undefined;
+        this.beforeAll(async function () {
+            const testFile = await TestExecutionHelper.createTestFileWith(TESTFILE_DIR_JAVA, "JavaWrapperVariableTestClass", "java", TestFileContents.ALL_WRAPPER_VARIABLES);
+
+            const context = await executeExtension(testFile);
+            result = await loadTraceFromContext(testFile, context);
+        });
+
+        it("should create a defined Backend Trace", () => {
+            assert.ok(result);
+        });
+
+        const variables: Array<[string, string, any, number, number]> = [
+            ['positiveByte', 'wrapper', 1, 1, 1],
+            ['negativeByte', 'wrapper', -1, 2, 1],
+            ['positiveShort', 'wrapper', 150, 3, 1],
+            ['negativeShort', 'wrapper', -150, 4, 1],
+            ['positiveInt', 'wrapper', 55555, 5, 1],
+            ['negativeInt', 'wrapper', -55555, 6, 1],
+            ['positiveLong', 'wrapper', 2947483647, 7, 1],
+            ['negativeLong', 'wrapper', -2947483647, 8, 1],
+            ['positiveFloat', 'wrapper', 1.0123, 9, 1],
+            ['negativeFloat', 'wrapper', -1.0123, 10, 1],
+            ['positiveDouble', 'wrapper', 1.01234, 11, 1],
+            ['negativeDouble', 'wrapper', -1.01234, 12, 1],
+            ['fullChar', 'wrapper', 'a', 13, 1],
+            ['trueBool', 'wrapper', 'true', 14, 1],
+            ['falseBool', 'wrapper', 'false', 15, 1],
+            ['emptyString', 'wrapper', '', 16, 2],
+            ['fullString', 'wrapper', 'Hello World!', 17, 2],
+        ];
+
+        variables.forEach(([name, type, value, references, elementCount], index) => {
+            it(`should contain the variable ${name} as ${type} with value ${value}`, () => {
+                if (!result) {
+                    assert.fail("No result was generated!");
+                }
+
+                const stack = result.at(index + 1)?.stack[0];
+                if (!stack) {
+                    assert.fail("No Stack Elements");
+                }
+
+                const heap = result.at(index + 1)?.heap;
+                if (!heap) {
+                    assert.fail("No Stack Elements");
+                }
+
+                stackAndHeapContainsVariable([name, type, references, elementCount], stack, heap);
+            });
+        });
+    });
+
+    describe("creating a Trace with Array-Variables", function () {
+        this.timeout(MAX_TEST_DURATION);
+
+        let result: BackendTrace | undefined;
+        this.beforeAll(async function () {
+            const testFile = await TestExecutionHelper.createTestFileWith(TESTFILE_DIR_JAVA, "JavaArrayVariableTestClass", "java", TestFileContents.ARRAY_VARIABLES);
+
+            const context = await executeExtension(testFile);
+            result = await loadTraceFromContext(testFile, context);
+        });
+
+        it("should create a defined Backend Trace", () => {
+            assert.ok(result);
+        });
+
+        const variables: Array<[string, string, any, number, number]> = [
+            ['intArray', 'int[]', '{ 1, 2, 3, 4}', 1, 4],
+            ['integerArray', 'Integer[]', '{ 1, 2, 3, 4}', 6, 4],
+            ['dimIntArray', 'int[][]', '{ { 1}, { 2}, { 3, 4}}', 10, 3],
+            ['dimIntegerArray', 'Integer[][]', '{ { 1}, { 2}, { 3, 4}}', 14, 3],
+        ];
+
+        variables.forEach(([name, type, value, references, elementCount], index) => {
+            it(`should contain the variable ${name} as ${type} with value ${value}`, () => {
+                if (!result) {
+                    assert.fail("No result was generated!");
+                }
+
+                const stack = result.at(index + 1)?.stack[0];
+                if (!stack) {
+                    assert.fail("No Stack Elements");
+                }
+
+                const heap = result.at(index + 1)?.heap;
+                if (!heap) {
+                    assert.fail("No Stack Elements");
+                }
+
+                stackAndHeapContainsVariable([name, type, references, elementCount], stack, heap);
+            });
+        });
+    });
+
+    describe("creating a Trace with Class", function () {
+        this.timeout(MAX_TEST_DURATION);
+
+        let result: BackendTrace | undefined;
+        this.beforeAll(async function () {
+            const testFile = await TestExecutionHelper.createTestFileWith(TESTFILE_DIR_JAVA, "JavaClassTestClass", "java", TestFileContents.CLASS_VARIABLES);
+
+            const context = await executeExtension(testFile);
+            result = await loadTraceFromContext(testFile, context);
+        });
+
+        it("should create a defined Backend Trace", () => {
+            assert.ok(result);
+        });
+
+        const variables: Array<[string, string, any, number, number]> = [
+            ['testClass', 'class', 'class simpleClass: x = 1; y = 2', 2, 2]
+        ];
+
+        variables.forEach(([name, type, value, references, elementCount], index) => {
+            it(`should contain the variable ${name} as ${type} with value ${value}`, () => {
+                if (!result) {
+                    assert.fail("No result was generated!");
+                }
+
+                const stack = result.at(-1)?.stack[0];
+                if (!stack) {
+                    assert.fail("No Stack Elements");
+                }
+
+                const heap = result.at(-1)?.heap;
+                if (!heap) {
+                    assert.fail("No Stack Elements");
+                }
+
+                stackAndHeapContainsVariable([name, type, references, elementCount], stack, heap);
+            });
+        });
+    });
+
+    describe("creating a Trace with ArrayList", function () {
+        this.timeout(MAX_TEST_DURATION);
+
+        let result: BackendTrace | undefined;
+        this.beforeAll(async function () {
+            const testFile = await TestExecutionHelper.createTestFileWith(TESTFILE_DIR_JAVA, "JavaArrayListTestClass", "java", TestFileContents.ARRAY_LIST_VARIABLES);
+
+            const context = await executeExtension(testFile);
+            result = await loadTraceFromContext(testFile, context);
+        });
+
+        it("should create a defined Backend Trace", () => {
+            assert.ok(result);
+        });
+
+        const variables: Array<[string, string, any, number, number]> = [
+            ['arrayList', 'ArrayList', '{ 3, 2, 1}', 4, 3]
+        ];
+
+        variables.forEach(([name, type, value, references, elementCount], index) => {
+            it(`should contain the variable ${name} as ${type} with value ${value}`, () => {
+                if (!result) {
+                    assert.fail("No result was generated!");
+                }
+
+                const stack = result.at(-1)?.stack[0];
+                if (!stack) {
+                    assert.fail("No Stack Elements");
+                }
+
+                const heap = result.at(-1)?.heap;
+                if (!heap) {
+                    assert.fail("No Stack Elements");
+                }
+
+                stackAndHeapContainsVariable([name, type, references, elementCount], stack, heap);
+            });
+        });
+    });
+
+    describe("creating a Trace with LinkedList", function () {
+        this.timeout(MAX_TEST_DURATION);
+
+        let result: BackendTrace | undefined;
+        this.beforeAll(async function () {
+            const testFile = await TestExecutionHelper.createTestFileWith(TESTFILE_DIR_JAVA, "JavaLinkedListTestClass", "java", TestFileContents.LINKED_LIST_VARIABLES);
+
+            const context = await executeExtension(testFile);
+            result = await loadTraceFromContext(testFile, context);
+        });
+
+        it("should create a defined Backend Trace", () => {
+            assert.ok(result);
+        });
+
+        const variables: Array<[string, string, any, number, number]> = [
+            ['linkedList', 'LinkedList', '{ 3, 2, 1}', 4, 3]
+        ];
+
+        variables.forEach(([name, type, value, references, elementCount], index) => {
+            it(`should contain the variable ${name} as ${type} with value ${value}`, () => {
+                if (!result) {
+                    assert.fail("No result was generated!");
+                }
+
+                const stack = result.at(-1)?.stack[0];
+                if (!stack) {
+                    assert.fail("No Stack Elements");
+                }
+
+                const heap = result.at(-1)?.heap;
+                if (!heap) {
+                    assert.fail("No Stack Elements");
+                }
+
+                stackAndHeapContainsVariable([name, type, references, elementCount], stack, heap);
+            });
+        });
+    });
+
+    describe("creating a Trace with HashMap", function () {
+        this.timeout(MAX_TEST_DURATION);
+
+        let result: BackendTrace | undefined;
+        this.beforeAll(async function () {
+            const testFile = await TestExecutionHelper.createTestFileWith(TESTFILE_DIR_JAVA, "JavaHashMapTestClass", "java", TestFileContents.HASH_MAP_VARIABLES);
+
+            const context = await executeExtension(testFile);
+            result = await loadTraceFromContext(testFile, context);
+        });
+
+        it("should create a defined Backend Trace", () => {
+            assert.ok(result);
+        });
+
+        const variables: Array<[string, string, any, number, number]> = [
+            ['hashMap', 'map', '{ {Audi : 1}, {BMW : 2}}', 5, 2]
+        ];
+
+        variables.forEach(([name, type, value, references, elementCount], index) => {
+            it(`should contain the variable ${name} as ${type} with value ${value}`, () => {
+                if (!result) {
+                    assert.fail("No result was generated!");
+                }
+
+                const stack = result.at(-1)?.stack[0];
+                if (!stack) {
+                    assert.fail("No Stack Elements");
+                }
+
+                const heap = result.at(-1)?.heap;
+                if (!heap) {
+                    assert.fail("No Stack Elements");
+                }
+
+                stackAndHeapContainsVariable([name, type, references, elementCount], stack, heap);
+            });
+        });
+    });
+
+    describe("creating a Trace with HashSet", function () {
+        this.timeout(MAX_TEST_DURATION);
+
+        let result: BackendTrace | undefined;
+        this.beforeAll(async function () {
+            const testFile = await TestExecutionHelper.createTestFileWith(TESTFILE_DIR_JAVA, "JavaHashSetTestClass", "java", TestFileContents.HASH_SET_VARIABLES);
+
+            const context = await executeExtension(testFile);
+            result = await loadTraceFromContext(testFile, context);
+        });
+
+        it("should create a defined Backend Trace", () => {
+            assert.ok(result);
+        });
+
+        const variables: Array<[string, string, any, number, number]> = [
+            ['hashSet', 'HashSet', '{ 1, 2 }', 3, 2]
+        ];
+
+        variables.forEach(([name, type, value, references, elementCount], index) => {
+            it(`should contain the variable ${name} as ${type} with value ${value}`, () => {
+                if (!result) {
+                    assert.fail("No result was generated!");
+                }
+
+                const stack = result.at(-1)?.stack[0];
+                if (!stack) {
+                    assert.fail("No Stack Elements");
+                }
+
+                const heap = result.at(-1)?.heap;
+                if (!heap) {
+                    assert.fail("No Stack Elements");
+                }
+
+                stackAndHeapContainsVariable([name, type, references, elementCount], stack, heap);
             });
         });
     });
@@ -69,4 +368,22 @@ function stackContainsVariable([name, type, value]: [string, string, string | nu
 
     assert(keys.includes(name));
     assert.deepEqual(values[keys.indexOf(name)], { type: type, value: value });
+}
+
+function stackAndHeapContainsVariable([name, type, references, elementCount]: [string, string, number, number], stack: any, heap: any) {
+    const stackKeys: any[] = Array.from(Object.keys(stack.locals));
+    const stackValues: any[] = Array.from(Object.values(stack.locals));
+
+    assert(stackKeys.includes(name));
+    assert.deepEqual(stackValues[stackKeys.indexOf(name)].type, 'ref');
+
+    const heapKeys: any[] = Array.from(Object.keys(heap));
+    const heapValues: any[] = Array.from(Object.values(heap));
+
+    const ref: number = stackValues[stackKeys.indexOf(name)].value;
+    assert(heapKeys.includes('' + ref));
+    assert.deepEqual(heapKeys.length, references);
+
+    assert.deepEqual(heapValues[heapKeys.indexOf('' + ref)].type, type);
+    assert.deepEqual(Object.keys(heapValues[heapKeys.indexOf('' + ref)].value).length, elementCount);
 }
